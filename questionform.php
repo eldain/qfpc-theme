95a2f562cf592f230ac2f6e273991cfb5337c4f6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-13 14:37:57 -0400

    stuff

--- questionform.php
+++ questionform.php
@@ -1,16 +1,18 @@
 <div class="question-form">
 
-  <div class="row">
-    <div class="col-centered">
+<div class="centered">
       Can't find what you're looking for? Ask a question below!
-    </div>
-  </div>
+</div>
 
   <?php
   if ( function_exists( 'ccf_output_form' ) ) {
     ccf_output_form(55);
   }
   ?>
+
+<div class="return-home">
+    <a class="return-home" href="/">Return to the home page</a>
+</div>
 
 </div>
 
