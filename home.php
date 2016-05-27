701da2fccd648d3d2421331591f50f20730a1880
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-05-27 15:57:34 -0400

    footer

--- home.php
+++ home.php
@@ -33,31 +33,19 @@
 
   </div>
 
-  <div class="row">
-
-    <div class="col-xs-5 col-sm-4 col-md-3 col-lg-3 category-icon">
-        <img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/kevork.jpg" alt="Kevork in Chair" style="border-radius: 3px;"/>
-    </div>
-
-    <!-- <div class="col-xs-5 col-sm-4 col-md-3 col-lg-3 category-icon">
-        <img class="img-responsive col-centered" src="<?php echo get_stylesheet_directory_uri(); ?>/images/kevork2.jpg" alt="Kevork in Chair" style="border-radius: 3px;"/>
-    </div> -->
-
-    <div class="col-xs-5 col-sm-4 col-md-3 col-lg-3 category-icon">
-        <p class="welcome-text">
-          "Welcome to UC Irvine’s Questions about Fundamental Physics Page!
-          <br><br>
-          The questions posted here originate from questions on fundamental physics and cosmology that have arisen from high school instructors and undergraduate students that have not had advanced courses. The answers are meant to be sufficiently non-technical so that they should be comprehensible to a person with high-school level physics and math.
-          <br><br>
-          There are questions in two broad and overlapping categories here: cosmology and particle physics. Click the icons above to see the question categories, search for questions on the search bar at the top, or submit a new if you can't find what you're looking for!"
-          <br><br>
-          -
-          <a href="http://www.physics.uci.edu/~kevork/Home.html">Kevork Abazajian</a>
-        </p>
-    </div>
-
+  <div class="row footer-container">
+      <img class="inchair img-responsive pull-left" src="<?php echo get_stylesheet_directory_uri(); ?>/images/kevork.jpg" alt="Kevork in Chair" />
+      <p class="welcome-text">
+        "Welcome to UC Irvine’s Questions about Fundamental Physics Page!
+        <br><br>
+        The questions posted here originate from questions on fundamental physics and cosmology that have arisen from high school instructors and undergraduate students that have not had advanced courses. The answers are meant to be sufficiently non-technical so that they should be comprehensible to a person with high-school level physics and math. This is an outreach website for the <a href="http://cosmology.uci.edu">Center for Cosmology</a> at UC Irvine.
+        <br><br>
+        There are questions in two broad and overlapping categories here: cosmology and particle physics. Click the icons above to see the question categories, search for questions on the search bar at the top, or submit a new if you can't find what you're looking for!"
+        <br><br>
+        -
+        <a href="http://www.physics.uci.edu/~kevork/Home.html">Kevork Abazajian</a>
+      </p>
   </div>
-
 <?//= get_template_part('questionform') ?>
 </div>
 
